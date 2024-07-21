d306b9b257a1ee93db409b2e8dde485e22310870
codewithsumit000 <159463216+codewithsumit000@users.noreply.github.com>
2024-07-21 21:17:36 +0545

    Add files via upload

---
+++ sumit.c
@@ -0,0 +1,10 @@
+//WAP to print the sum of two number given by user
+#include<stdio.h>
+int main()
+{
+    double first_Number,second_Number,sum;
+    printf("enter the two number you want \n:");
+    scanf("%lf%lf",&first_Number,&second_Number);
+    sum=first_Number+second_Number;
+    printf("sum=%lf",sum)
+}
